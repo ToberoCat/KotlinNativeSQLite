9d746e368097eae49593a027be464b1f345e9c2a
Tobias Madlberger <tobias.madlbeger@htlstp.at>
2024-01-24 09:03:56 +0100

    updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.9.20-RC2"
+    kotlin("multiplatform") version "1.9.22"
+    id("app.cash.sqldelight") version "2.0.1"
 }
 
 group = "me.user"
@@ -7,6 +8,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+sqldelight {
+    databases {
+        create("Database") {
+            packageName.set("com.example")
+        }
+    }
 }
 
 kotlin {
@@ -30,7 +39,11 @@ kotlin {
         }
     }
     sourceSets {
-        val nativeMain by getting
+        val nativeMain by getting {
+            dependencies {
+                implementation("app.cash.sqldelight:native-driver:2.0.1")
+            }
+        }
         val nativeTest by getting
     }
 }
--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,3 +1,20 @@
 fun main() {
     println("Hello, Kotlin/Native!")
+    val driver: SqlDriver = NativeSqliteDriver(Database.Schema, "test.db")
+    doDatabaseThings(driver)
+}
+
+fun doDatabaseThings(driver: SqlDriver) {
+    val database = Database(driver)
+    val playerQueries: PlayerQueries = database.playerQueries
+
+    println(playerQueries.selectAll().executeAsList())
+    // [HockeyPlayer(15, "Ryan Getzlaf")]
+
+    playerQueries.insert(player_number = 10, full_name = "Corey Perry")
+    println(playerQueries.selectAll().executeAsList())
+    // [HockeyPlayer(15, "Ryan Getzlaf"), HockeyPlayer(10, "Corey Perry")]
+
+    val player = HockeyPlayer(10, "Ronald McDonald")
+    playerQueries.insertFullPlayerObject(player)
 }
